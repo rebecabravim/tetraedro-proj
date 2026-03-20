7a2a034ae1bf5ceb5f4df30bdbf83e2c8ae39c50
rebecabg <rebecabrgarcia@gmail.com>
2026-03-20 14:34:30 -0300

    adiciona rotacoes dos grupos a partir das teclas f1-f12; computar os planos que passam pelo baricentro dos grupos; troca 'criarTetra' por 'createTetra';

--- Assets/managerEu.cs
+++ Assets/managerEu.cs
@@ -1,20 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class managerEu : MonoBehaviour {
 
     public GameObject tetrahedron; // prefab da camrera
     public GameObject[] vetGameObj = new GameObject[24];
+    public GameObject plano;
+
+    GameObject planoTP0;
+    GameObject planoMP0;
+    GameObject planoBP0;
+
+    GameObject planoTP1;
+    GameObject planoMP1;
+    GameObject planoBP1;
+
+    GameObject planoTP2;
+    GameObject planoMP2;
+    GameObject planoBP2;
+
+    GameObject planoTP3;
+    GameObject planoMP3;
+    GameObject planoBP3;
+
+    
+
     GameObject pai;
     Vector3 eixoP0World;
     Vector3 eixoP1World;
     Vector3 eixoP2World;
     Vector3 eixoP3World;
-    int[] tetraredrosMeioEixoP3 = new int[] { 3, 4, 8, 12, 18, 22 };
-    int[] tetraredrosBaseEixoP3 = new int[] { 0, 1, 2, 6, 7, 9, 10, 11, 13, 14, 15, 16, 17, 19, 20, 21 };
+
+    int[] tetraredrosTopoEixoP0 = new int[] { 0 };
+    int[] tetraredrosMeioEixoP0 = new int[] { 1, 3, 9, 10, 13, 17 };
+    int[] tetraredrosBaseEixoP0 = new int[] { 2, 4, 5, 6, 7, 8, 11, 12, 14, 15, 16, 18, 19, 20, 21 };
+
+    int[] tetraredrosTopoEixoP1 = new int[] { 2 };
+    int[] tetraredrosMeioEixoP1 = new int[] { 1, 4, 6, 11, 14, 19 };
+    int[] tetraredrosBaseEixoP1 = new int[] { 0, 3, 5, 7, 8, 9, 10, 12, 13, 15, 16, 17, 18, 20, 21 };
+
+    int[] tetraredrosTopoEixoP2 = new int[] { 7 };
+    int[] tetraredrosMeioEixoP2 = new int[] { 6, 8, 9, 15, 16, 20 };
+    int[] tetraredrosBaseEixoP2 = new int[] { 0, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 17, 18, 19, 21 };
+
+    int[] tetraredrosTopoEixoP3 = new int[] { 5 };
+    int[] tetraredrosMeioEixoP3 = new int[] { 3, 4, 8, 12, 18, 21 };
+    int[] tetraredrosBaseEixoP3 = new int[] { 0, 1, 2, 6, 7, 9, 10, 11, 13, 14, 15, 16, 17, 19, 20 };
 
 
 
@@ -22,7 +58,13 @@ public class managerEu : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		for(int i=0; i < 24; i++)
+        //planoTP3 = Instantiate(plano, new Vector3(0, 2.4f, 0), Quaternion.identity);
+        //planoMP3 = Instantiate(plano, new Vector3(0, 1.4f, 0), Quaternion.identity);
+        //planoBP3 = Instantiate(plano, new Vector3(0,0.4f,0), Quaternion.identity);
+
+
+
+        for (int i=0; i < 24; i++)
         {
             if(i == 0)
             {
@@ -69,7 +111,7 @@ public class managerEu : MonoBehaviour {
         pai.transform.Rotate(Vector3.down, 60f);
         vetGameObj[16].transform.parent = pai.transform;
         pai.transform.Rotate(Vector3.forward, 180f);
-        pai.transform.Rotate(Vector3.right, 36.87f);
+        pai.transform.Rotate(Vector3.right, 37f);
         vetGameObj[16].transform.parent = null;
         pai.transform.rotation = Quaternion.identity;
 
@@ -78,7 +120,7 @@ public class managerEu : MonoBehaviour {
         pai.transform.Rotate(Vector3.down, 60f);
         vetGameObj[17].transform.parent = pai.transform;
         pai.transform.Rotate(Vector3.forward, 180f);
-        pai.transform.Rotate(Vector3.right, 36.87f);
+        pai.transform.Rotate(Vector3.right, 37f);
         vetGameObj[17].transform.parent = null;
         pai.transform.rotation = Quaternion.identity;
 
@@ -87,7 +129,7 @@ public class managerEu : MonoBehaviour {
         pai.transform.Rotate(Vector3.down, 60f);
         vetGameObj[18].transform.parent = pai.transform;
         pai.transform.Rotate(Vector3.forward, 180f);
-        pai.transform.Rotate(Vector3.right, 36.87f);
+        pai.transform.Rotate(Vector3.right, 37f);
         vetGameObj[18].transform.parent = null;
         pai.transform.rotation = Quaternion.identity;
 
@@ -98,7 +140,7 @@ public class managerEu : MonoBehaviour {
         pai.transform.Rotate(Vector3.up, 60f);
         vetGameObj[19].transform.parent = pai.transform;
         pai.transform.Rotate(Vector3.forward, 180f);
-        pai.transform.Rotate(Vector3.right, 36.87f);
+        pai.transform.Rotate(Vector3.right, 37f);
         vetGameObj[19].transform.parent = null;
         pai.transform.rotation = Quaternion.identity;
 
@@ -107,26 +149,17 @@ public class managerEu : MonoBehaviour {
         pai.transform.Rotate(Vector3.up, 60f);
         vetGameObj[20].transform.parent = pai.transform;
         pai.transform.Rotate(Vector3.forward, 180f);
-        pai.transform.Rotate(Vector3.right, 36.87f);
+        pai.transform.Rotate(Vector3.right, 37f);
         vetGameObj[20].transform.parent = null;
         pai.transform.rotation = Quaternion.identity;
 
-        vetGameObj[21].transform.position = new Vector3(0.5f * 3, 0.86603f, 0.28868f);
-        pai.transform.position = new Vector3(0.5f * 4, 0.86603f, 0.28868f + Mathf.Sqrt(3) / 2);
+        vetGameObj[21].transform.position = new Vector3(0.5f * 3, 0.86603f * 2, -0.28868f);
+        pai.transform.position = new Vector3(2f, 0.86603f * 2, 0.28868f * 2);
         pai.transform.Rotate(Vector3.up, 60f);
         vetGameObj[21].transform.parent = pai.transform;
         pai.transform.Rotate(Vector3.forward, 180f);
-        pai.transform.Rotate(Vector3.right, 36.87f);
+        pai.transform.Rotate(Vector3.right, 37f);
         vetGameObj[21].transform.parent = null;
-        pai.transform.rotation = Quaternion.identity;
-
-        vetGameObj[22].transform.position = new Vector3(0.5f * 3, 0.86603f * 2, -0.28868f);
-        pai.transform.position = new Vector3(2f, 0.86603f * 2, 0.28868f * 2);
-        pai.transform.Rotate(Vector3.up, 60f);
-        vetGameObj[22].transform.parent = pai.transform;
-        pai.transform.Rotate(Vector3.forward, 180f);
-        pai.transform.Rotate(Vector3.right, 36.87f);
-        vetGameObj[22].transform.parent = null;
         pai.transform.rotation = Quaternion.identity;
 
         // PARTE 1: COMPUTAR 4 EIXOS DE ROTAÇÃO =============================================================================
@@ -154,8 +187,31 @@ public class managerEu : MonoBehaviour {
         Vector3 eixoP3 = (P3 - baricentro).normalized; // esse é o Eixo Vertical
         eixoP3World = eixoP3.normalized;
 
-        // comentado, pois estou chamando a função no update
-        //RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosBaseEixoP3, 120, eixoP3World);
+
+        // PARTE 2: COMPUTAR OS PLANOS QUE PASSAM PELO BARICENTRO DOS GRUPOS ===============================================
+
+        Quaternion rotPlanoP0 = CalcularRotacaoDoPlanoAPartirDoIndiceDe3Tetraedros(1, 3, 9);
+        planoTP0 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosTopoEixoP0), rotPlanoP0);
+        planoMP0 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosMeioEixoP0), rotPlanoP0);
+        planoBP0 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosBaseEixoP0), rotPlanoP0);
+
+        Quaternion rotPlanoP1 = CalcularRotacaoDoPlanoAPartirDoIndiceDe3Tetraedros(1, 4, 6);
+        planoTP1 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosTopoEixoP1), rotPlanoP1);
+        planoMP1 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosMeioEixoP1), rotPlanoP1);
+        planoBP1 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosBaseEixoP1), rotPlanoP1);
+
+        Quaternion rotPlanoP2 = CalcularRotacaoDoPlanoAPartirDoIndiceDe3Tetraedros(6, 8, 9);
+        planoTP2 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosTopoEixoP2), rotPlanoP2);
+        planoMP2 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosMeioEixoP2), rotPlanoP2);
+        planoBP2 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosBaseEixoP2), rotPlanoP2);
+
+        // P3 é vertical, logo nao precisa calcular rotacao do plano
+        planoTP3 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosTopoEixoP3), Quaternion.identity);
+        planoMP3 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosMeioEixoP3), Quaternion.identity);
+        planoBP3 = Instantiate(plano, CalcularBaricentroListaTetraedros(tetraredrosBaseEixoP3), Quaternion.identity);
+
+
+
 
 
     }
@@ -182,7 +238,7 @@ public class managerEu : MonoBehaviour {
     // Retorna o baricentro do tetraedro em coordenadas globais.
     private Vector3 CalcularBaricentroWorld(GameObject tetra)
     {
-        criarTetra ct = tetra.GetComponent<criarTetra>();
+        createTetra ct = tetra.GetComponent<createTetra>();
         if (ct == null)
         {
             Debug.LogError("O GameObject " + tetra.name + " não possui createTetra.");
@@ -241,16 +297,80 @@ public class managerEu : MonoBehaviour {
         }
     }
 
-    // Update is called once per frame
-    void Update () {
-        //aqui chama rotacionar 
-        RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(
-                                                                tetraredrosBaseEixoP3,
-                                                                30f * Time.deltaTime,
-                                                                eixoP3World);
-        RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(
-                                                                tetraredrosMeioEixoP3,
-                                                                40f * Time.deltaTime,
-                                                                eixoP3World);
+    // Calcula a rotação de um plano a partir de três tetraedros.
+    Quaternion CalcularRotacaoDoPlanoAPartirDoIndiceDe3Tetraedros(int i1, int i2, int i3)
+    {
+        Vector3 p1 = CalcularBaricentroWorld(vetGameObj[i1]);
+        Vector3 p2 = CalcularBaricentroWorld(vetGameObj[i2]);
+        Vector3 p3 = CalcularBaricentroWorld(vetGameObj[i3]);
+
+        Vector3 v1 = p2 - p1;
+        Vector3 v2 = p3 - p1;
+
+        Vector3 normal = Vector3.Cross(v1, v2).normalized;
+
+        return Quaternion.FromToRotation(Vector3.up, normal);
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ===== EIXO P0 =====
+        if (Input.GetKeyDown(KeyCode.F1))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosBaseEixoP0, 120f, eixoP0World);
+
+        if (Input.GetKeyDown(KeyCode.F2))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosMeioEixoP0, 120f, eixoP0World);
+
+        if (Input.GetKeyDown(KeyCode.F3))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosTopoEixoP0, 120f, eixoP0World);
+
+
+        // ===== EIXO P1 =====
+        if (Input.GetKeyDown(KeyCode.F4))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosBaseEixoP1, 120f, eixoP1World);
+
+        if (Input.GetKeyDown(KeyCode.F5))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosMeioEixoP1, 120f, eixoP1World);
+
+        if (Input.GetKeyDown(KeyCode.F6))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosTopoEixoP1, 120f, eixoP1World);
+
+
+        // ===== EIXO P2 =====
+        if (Input.GetKeyDown(KeyCode.F7))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosBaseEixoP2, 120f, eixoP2World);
+
+        if (Input.GetKeyDown(KeyCode.F8))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosMeioEixoP2, 120f, eixoP2World);
+
+        if (Input.GetKeyDown(KeyCode.F9))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosTopoEixoP2, 120f, eixoP2World);
+
+
+        // ===== EIXO P3 =====
+        if (Input.GetKeyDown(KeyCode.F10))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosBaseEixoP3, 120f, eixoP3World);
+
+        if (Input.GetKeyDown(KeyCode.F11))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosMeioEixoP3, 120f, eixoP3World);
+
+        if (Input.GetKeyDown(KeyCode.F12))
+            RotacionarGrupoEmTornoDoEixoQuePassaNoBaricentroDoGrupo(tetraredrosTopoEixoP3, 120f, eixoP3World);
+    }
+
+    int[] TeatraedrosNoPlano(GameObject plano) 
+    {
+        int[] tetraedros = new int[] { };
+
+        foreach (GameObject tetraedro in vetGameObj) 
+        { 
+            //if (tetraedro != plano)
+
+        }
+
+
+        return new int[] { };
+    }
+    
 }
